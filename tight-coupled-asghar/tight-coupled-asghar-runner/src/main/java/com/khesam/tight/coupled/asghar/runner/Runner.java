868fd411450187648f5d825c9c862c73baee1497
khesam109 <khesam109@gmail.com>
2023-11-04 13:20:33 -0700

    fix bug, add loose-coupled module

--- tight-coupled-asghar/tight-coupled-asghar-runner/src/main/java/com/khesam/tight/coupled/asghar/runner/Runner.java
+++ tight-coupled-asghar/tight-coupled-asghar-runner/src/main/java/com/khesam/tight/coupled/asghar/runner/Runner.java
@@ -1,18 +1,18 @@
 package com.khesam.tight.coupled.asghar.runner;
 
 
-//import com.khesam.tight.coupled.asghar.swing.ui.AsgharSwingView;
 
 import com.khesam.tight.coupled.asghar.console.ui.AsgharConsoleView;
+import com.khesam.tight.coupled.asghar.swing.ui.AsgharSwingView;
 
 public class Runner {
 
     public static void main(String[] args) {
 
-        AsgharConsoleView asgharConsoleView = new AsgharConsoleView();
-        asgharConsoleView.showAsghars();
+//        AsgharConsoleView asgharConsoleView = new AsgharConsoleView();
+//        asgharConsoleView.showAsghars();
 
-//        AsgharSwingView asgharSwingView = new AsgharSwingView();
-//        asgharSwingView.showAsghars();
+        AsgharSwingView asgharSwingView = new AsgharSwingView();
+        asgharSwingView.showAsghars();
     }
 }
--- tight-coupled-asghar/tight-coupled-asghar-swing-ui/src/main/java/com/khesam/tight/coupled/asghar/swing/ui/AsgharSwingView.java
+++ tight-coupled-asghar/tight-coupled-asghar-swing-ui/src/main/java/com/khesam/tight/coupled/asghar/swing/ui/AsgharSwingView.java
@@ -1,14 +1,25 @@
 package com.khesam.tight.coupled.asghar.swing.ui;
 
+import com.khesam.tight.coupled.asghar.domain.AsgharService;
+
 import javax.swing.*;
+import java.util.stream.Collectors;
 
 public class AsgharSwingView {
+
+    private final AsgharService asgharService;
+
+    public AsgharSwingView() {
+        this.asgharService = new AsgharService();
+    }
 
     public void showAsghars() {
         JFrame frame = new JFrame("Asghars!");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(300,300);
-        JTextArea jTextArea = new JTextArea("salam");
+        JTextArea jTextArea = new JTextArea(
+                this.asgharService.getAllAsghar().stream().map(e -> e.toString() + "\n").collect(Collectors.joining())
+        );
         frame.getContentPane().add(jTextArea);
         frame.setVisible(true);
     }
